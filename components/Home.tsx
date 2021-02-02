596e73ee9b97fe045a4b4f23d4fcef02699fda15
mlounnas <lounnas.maxime@gmail.com>
2021-02-02 10:52:03 +0100

    Nico

--- components/Home.tsx
+++ components/Home.tsx
@@ -1,27 +1,46 @@
 import React, { Component } from 'react';
-import { Container, Content, Header, Footer, FooterTab, Button, Text } from 'native-base';
 import DisplayAnImage from '../components/Pictures';
 import HomeForm from '../components/HomeForm';
+import {Button} from "react-native";
+import firestore, { firebase } from '@react-native-firebase/firestore';
+import {Container,Header,Content,Footer,FooterTab} from 'native-base';
 
-export default class Home extends Component {
-  render() {
+
+const home = (props : any)=> {
+  const config : any ={};
+
+  const ref = firestore().collection('liste');
+        
+       async function testDB(){ await ref.add({
+
+    nom:'tarace'
+
+   })
+   .then(function (docRef) {
+
+    console.log("Document written with ID: ", docRef.id);
+
+  })
+    .catch(function (error) {
+      console.error("Error adding document: ", error);
+    });}
     return (
-      <>
+      
         <Container>
           <Header>
             <DisplayAnImage />
           </Header>
-          <Content />
-          <HomeForm />
+          <Content/>
+          <HomeForm/>
           <Footer>
             <FooterTab>
-              <Button full>
-                <Text>Copyright ShareCook 2021</Text>
-              </Button>
+              <Button title='Copyright ShareCook 2021' onPress={(e:any) => (testDB())}/>
+                
+             
             </FooterTab>
           </Footer>
         </Container>
-      </>
+      
     );
   }
-}
+  export default home;
